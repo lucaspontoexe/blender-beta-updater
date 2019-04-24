d022940a6bc8e8e9ba21e9d95713cb90f3b6ce90
lucaspontoexe <ltx16@hotmail.com>
2019-04-24 13:11:51 -0300

    Initial Release

---
+++ download_blender_28.py
@@ -0,0 +1,44 @@
+import os
+import urllib.request
+import subprocess
+from bs4 import BeautifulSoup
+#import zipfile
+
+print("Procurando... ")
+
+html = urllib.request.urlopen("https://builder.blender.org/download").read()
+soup = BeautifulSoup(html, 'html.parser')
+site = "https://builder.blender.org"
+name = "blender2.8.zip"
+
+#for link in soup.find_all('a'):
+#    print(link.get('href'))
+
+print("Baixando... ")
+print("(uma progress bar até que cairia bem agora, né)")
+urllib.request.urlretrieve(site + soup.find_all('a')[9].get('href'), name)
+print("Pronto.")
+
+#print("Extract it yourself")
+#with zipfile.ZipFile(name) as thedamnfile:
+#    print("(well, why am I writing this in English?)")
+#    thedamnfile.extractall()
+#    print("All done.")
+
+print("Extraindo...")
+subprocess.run(["7z", "x", name])
+print("Extraído. Atualizando...")
+
+#rename app to old-something
+try:
+	hashingbarato = "C:\\Users\\ltx16\\AppData\\Local\\Temp\\old " + str(os.path.getctime("app"))
+	os.rename("app", hashingbarato)
+except:
+	print("Não tinha versão anterior aqui. Continuando...")
+
+#rename blender to app
+for item in os.listdir():
+	if "blender-2.80" in item:
+		os.rename(item, "app")
+			
+print("tá pronto 👍 100% atualizado")
